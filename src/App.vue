15035dea6ac36c41c2ba4a7eb5d0133719f93626
Rayanda Wiyoga Belva <rayanda.belva99@gmail.com>
2024-04-07 22:42:45 +0700

    FIX

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="mt-5 text-center mb-3">Todos Data</h1>
+    <li class="nav-item active">
+      <button href="Add.vue" type="button" class="btn btn-primary">Add</button>
+    </li>
+    <table class="table">
+      <thead>
+        <tr class="header">
+          <th>ID</th>
+          <th>Title</th>
+          <th>Completed</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in list_paged" :key="item.id" class="item">
+          <td>{{ item.id }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.completed }}</td>
+          <td>
+            <button href="App.vue" type="button" class="btn btn-primary">Update</button>
+            <button href="App.vue" type="button" class="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+      <div class="column">
+        <p>
+          Data <b>{{ begin+1 }} - {{ end >= list.length ? list.length : end }}</b>
+          from <b>{{ list.length }}</b> data
+        </p>
+        <div class="column has-text-right">
+          <button class="bi bi-arrow-left" @click="pageNav('left')" :disabled="begin===0">◀</button>
+          <button class="bi bi-arrow-right" @click="pageNav('right')" :disabled="end >= list.length">▶</button>
+        </div>
+      </div>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      list: [],
+      list_paged: [],
+      begin: 0,
+      end: 10
+    }
+  },
+
+  async mounted() {
+    let result = await axios.get("https://jsonplaceholder.typicode.com/todos/");
+    this.list = result.data;
+    this.pageNav(); 
+  },
+  
+  methods: {
+    pageNav(direction) {
+      if (direction === 'left' && this.begin > 0) {
+        this.begin -= 10;
+        this.end -= 10;
+      } else if (direction === 'right' && this.end < this.list.length) {
+        this.begin += 10;
+        this.end += 10;
+      }
+      this.list_paged = this.list.slice(this.begin, this.end);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.header th,
+.item td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+.header th {
+  background-color: #f2f2f2;
 }
 </style>
